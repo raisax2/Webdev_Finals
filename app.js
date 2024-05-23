c73776dcb339ce6f2484282c5c0fe5c8b4163a54
Raisa Methila <92753269+raisax2@users.noreply.github.com>
2024-05-23 18:16:36 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// apps.js file
+// app.js file
 
 const express = require('express');
 const axios = require('axios');
@@ -8,10 +8,24 @@ const port = 3000;
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
+// Function to detect if a string contains English words
+function isEnglish(text) {
+  // Regular expression to match English words
+  const englishRegex = /^[A-Za-z0-9!@#$%^&*(),.?":{}|<> ]*$/;
+  return englishRegex.test(text);
+}
+
 app.get('/', async (req, res) => {
   try {
-    const response = await axios.get('https://cat-fact.herokuapp.com/facts/random');
-    const randomFact = response.data.text;
+    let randomFact;
+    let isEnglishFact = false;
+
+    while (!isEnglishFact) {
+      const response = await axios.get('https://cat-fact.herokuapp.com/facts/random');
+      randomFact = response.data.text;
+      isEnglishFact = isEnglish(randomFact);
+    }
+
     res.render('index', { fact: randomFact });
   } catch (error) {
     console.error('Error fetching cat facts:', error.message);
